5caa586bc693785d878a6ca3d04baf6e7e1d0ee6
jaime <81280780+ishinesky@users.noreply.github.com>
2026-07-01 15:10:15 +0800

    feat: export source quality service

--- ColumnMVP/__init__.py
+++ ColumnMVP/__init__.py
@@ -9,6 +9,7 @@ from .models import ColumnConfig, SourceConfig, TopicBrief, ValidationResult
 from .column_store import ColumnStore
 from .column_service import ColumnService
 from .source_candidate_service import SourceCandidateService
+from .source_quality import SourceQualityReport, SourceQualityService
 from .source_search_provider import SourceSearchCandidate, get_source_search_provider
 from .web_service_integration import register_opc_column_routes, patch_intelligence_hub_web_service
 
@@ -20,6 +21,8 @@ __all__ = [
     "ColumnStore",
     "ColumnService",
     "SourceCandidateService",
+    "SourceQualityReport",
+    "SourceQualityService",
     "SourceSearchCandidate",
     "get_source_search_provider",
     "register_opc_column_routes",
